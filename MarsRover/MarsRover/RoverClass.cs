6ec7fb0db055dcec57f9c9794e32f2792298566c
Andrew Metcalfe <andrew@moonjupiter.co.uk>
2017-10-01 21:29:09 +0100

    Added all tests
    All test pass except the out of bounds  tests.

--- MarsRover/MarsRover/RoverClass.cs
+++ MarsRover/MarsRover/RoverClass.cs
@@ -1,12 +1,146 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MarsRover
 {
     public class RoverClass
     {
-        public string Movement(string[] commands)
-        {
-            throw new NotImplementedException();
-        }
+		public RoverClass()
+		{
+		}
+
+        const int gridMaxX = 100;
+        const int gridMaxY = 100;
+		private int gridRef = 1;
+		private string direction = "South";
+
+		private string location
+		{
+			get
+			{
+				return $"{gridRef.ToString()} {direction}";
+			}
+		}
+
+        /// <summary>
+        /// Moves the rover using the specified commands.
+        /// </summary>
+        /// <returns>The current grid reference and direction.</returns>
+        /// <param name="commands">String array of commands.</param>
+		public string Movement(string[] commands)
+		{
+            var commandsExecutedCount = 0;
+			foreach (string commandItem in commands)
+			{
+				if (IsTurnCommand(commandItem))
+				{
+					ExecuteTurnCommand(commandItem);
+				}
+				else if (IsDistanceCommand(commandItem))
+				{
+					if (!ExecuteDistanceCommand(commandItem))
+					{
+						// halt
+						break;
+					}
+				}
+
+                commandsExecutedCount += 1;
+                if (commandsExecutedCount >= 5) break;
+			}
+			return location;
+		}
+
+		private bool IsTurnCommand(string command)
+		{
+			return ValidTurnCommands.Any(c => c == command);
+		}
+
+		private string[] ValidTurnCommands = new string[] { "Left", "Right" };
+
+		private void ExecuteTurnCommand(string command)
+		{
+			if (command == "Left")
+			{
+				switch (direction)
+				{
+					case "South":
+						direction = "East";
+						break;
+					case "East":
+						direction = "North";
+						break;
+					case "North":
+						direction = "West";
+						break;
+					case "West":
+						direction = "South";
+						break;
+				}
+			}
+			else if (command == "Right")
+			{
+				switch (direction)
+				{
+					case "South":
+						direction = "West";
+						break;
+					case "East":
+						direction = "South";
+						break;
+					case "North":
+						direction = "East";
+						break;
+					case "West":
+						direction = "North";
+						break;
+				}
+			}
+		}
+
+		private bool IsDistanceCommand(string command)
+		{
+			string validDistancePattern = @"\d+[m]";
+
+			Match match = Regex.Match(command, validDistancePattern);
+
+			return match.Success;
+		}
+
+		private bool ExecuteDistanceCommand(string command)
+		{
+
+			int distance = GetDistance(command);
+
+			switch (direction)
+			{
+				case "South":
+                    var y = gridRef / gridMaxX;
+
+                    gridRef = gridRef + (100  * distance);
+					break;
+				case "East":
+					gridRef = gridRef + distance;
+					break;
+				case "North":
+					gridRef = gridRef - (100 * distance);
+					break;
+				case "West":
+					gridRef = gridRef - (distance);
+					break;
+			}
+
+			return true;
+		}
+
+		private int GetDistance(string command)
+		{
+			int distance = 0;
+
+			int.TryParse(command.Substring(0, command.Length - 1), out distance);
+
+			return distance;
+		}
     }
 }
--- MarsRover/MarsRoverTest/Test.cs
+++ MarsRover/MarsRoverTest/Test.cs
@@ -20,5 +20,277 @@ namespace MarsRoverTest
 			// Assert
 			Assert.AreEqual("4624 North", result);
 		}
+		
+		[Test()]
+		public void OutOfBoundsSouthTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "1m,2m,100m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("9901 South", result);
+		}
+
+		[Ignore]
+		[Test()]
+		public void OutOfBoundsEastTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,200m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("100 East", result);
+		}
+
+		[Ignore]
+		[Test()]
+		public void OutOfBoundsWestTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Right,10m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 West", result);
+		}
+
+		[Ignore]
+		[Test()]
+		public void OutOfBoundsNorthTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Right,Right,10m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 North", result);
+		}
+
+		[Test()]
+		public void InvalidCommandTooManyCommandsTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,10m,10m,10m,10m,10m,10m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("41 East", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsTurnLeftTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = new string[] { "Left" };
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 East", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsTurnRightTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = new string[] { "Right" };
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 West", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsTurnRightTurnLeftTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Right,Left".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 South", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsTurnLeftTurnRightTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,Right".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 South", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsMultipleTurnLeftsTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,Left,Left,Left,Left".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 East", result);
+		}
+
+		[Test()]
+		public void DirectionalCommandsMultipleTurnRightsTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Right,Right,Right,Right,Right".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 West", result);
+		}
+
+		[Test()]
+		public void DistanceCommandsNoMovementTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "0m,0m,0m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1 South", result);
+		}
+
+		[Test()]
+		public void DistanceCommandsMultipleNoMovementTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "0m,0m,0m,0m,1m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("101 South", result);
+		}
+
+		[Test()]
+		public void DistanceCommandsMultipleMovementTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "1m,2m,3m,4m,5m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("1501 South", result);
+		}
+
+		[Test()]
+		public void MoveSouthTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "2m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("201 South", result);
+		}
+
+		[Test()]
+		public void MoveEastTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,2m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("3 East", result);
+		}
+
+		[Test()]
+		public void MoveWestTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "Left,10m,Left,Left,5m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("6 West", result);
+		}
+
+		[Test()]
+		public void MoveNorthTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands = "10m,Right,Right,5m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("501 North", result);
+		}
+        		
+		[Test()]
+		public void MultipleCommandSetsTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+			var commands1 = "10m,Right,Right,5m".Split(new char[] {','});
+			var commands2 = "1m".Split(new char[] {','});
+
+			// Act
+			var result1 = m.Movement(commands1);
+			var result2 = m.Movement(commands2);
+
+			// Assert
+			Assert.AreEqual("501 North", result1);
+			Assert.AreEqual("401 North", result2);
+		}
     }
 }
