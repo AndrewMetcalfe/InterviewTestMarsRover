f8c209bb613714e065646cc144ab391c31afe7e0
Andrew Metcalfe <andrew@moonjupiter.co.uk>
2017-10-01 20:45:03 +0100

    Initial commit - Stub RoverClass and 1 test

---
+++ MarsRover/MarsRover/RoverClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class RoverClass
+    {
+        public string Movement(string[] commands)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRover/MarsRoverTest/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using MarsRover;
+
+namespace MarsRoverTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+		public void GivenExampleTest()
+		{
+			// Arrange 
+			var m = new RoverClass();
+            var commands = "50m,Left,23m,Left,4m".Split(new char[] {','});
+
+			// Act
+			var result = m.Movement(commands);
+
+			// Assert
+			Assert.AreEqual("4624 North", result);
+		}
+    }
+}
